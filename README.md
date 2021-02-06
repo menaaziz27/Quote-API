47aa7bb471aa044159d037ee96347ca0e990f837
Azzouz <menaaziz27@gmail.com>
2021-02-06 20:07:02 +0200

    README.md & welcome route

---
+++ README.md
@@ -0,0 +1,5 @@
+# Quotes-API
+
+I created this API to use it later for my development for testing purposes or whatever the application I'm working on. It's express API, created with mongoose deployed in MongoDB and heroku.
+
+[https://apiforquotes.herokuapp.com](https://apiforquotes.herokuapp.com)
--- app.js
+++ app.js
@@ -16,6 +16,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('tiny'));
 app.use(cors());
 
+app.use('/', (req, res, next) => {
+	res.json({ message: 'Welcome to my quotes API :)!' });
+});
 app.use(quoteRoutes);
 app.use((req, res, next) => {
 	res.json({ error: 'not exist' });
