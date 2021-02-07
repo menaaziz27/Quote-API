96d73a705db399ba15bdf8e74d2a57db7214cfde
Azzouz <menaaziz27@gmail.com>
2021-02-07 10:07:40 +0200

    welcome page

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('tiny'));
 app.use(cors());
 
-app.use('/', (req, res, next) => {
+app.get('/', (req, res, next) => {
 	res.json({ message: 'Welcome to my quotes API :)!' });
 });
 app.use(quoteRoutes);
